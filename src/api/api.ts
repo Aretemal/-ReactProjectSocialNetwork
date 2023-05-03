3faf8c58ac8f811f068e90d3ec6e8b11ac17bce9
Aretemal <arproggamoi@gmail.com>
2023-05-03 23:16:28 +0300

    SN-FRONT-28 feat: update hook useSelector and useDispatch, add types.d.ts, tsconfig.json

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,61 +4,61 @@ const baseInstance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   headers: {},
 });
-const makeInstanceWithToken = (token) => axios.create({
+const makeInstanceWithToken = (token: string) => axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   headers: { Authorization: token },
 });
 export const dialogAPI = {
-  sendMessage({ token, id, message }) {
+  sendMessage({ token, id, message }: { id: string, token: string, message: string }) {
     return makeInstanceWithToken(token).post(`dialog/message/${id}`, { message });
   },
-  getAllMessages({ token, id }) {
+  getAllMessages({ token, id }: { id: string, token: string }) {
     return makeInstanceWithToken(token).get(`dialog/messages/${id}`);
   },
-  getAllDialogs(token) {
+  getAllDialogs(token: string) {
     return makeInstanceWithToken(token).get('dialog/companion');
   },
 };
 export const profileAPI = {
-  getInfoAuthUser(token) {
+  getInfoAuthUser(token: string) {
     return makeInstanceWithToken(token).get('profile/user');
   },
-  updateStatus({ status, token }) {
+  updateStatus({ status, token }: { status: string, token: string }) {
     return makeInstanceWithToken(token).put('profile/status', { status });
   },
 };
 export const postAPI = {
-  getAllPosts(token) {
+  getAllPosts(token: string) {
     return makeInstanceWithToken(token).get('profile/posts');
   },
-  addPost({ newMessageText, token }) {
+  addPost({ newMessageText, token }: { newMessageText: string, token: string }) {
     return makeInstanceWithToken(token).post('profile/posts', { newMessageText });
   },
 };
 export const usersAPI = {
-  requestUsers({ currentPage, pageSize, token }) {
+  requestUsers({ currentPage, pageSize, token }: { pageSize: number, currentPage: number, token: string }) {
     return makeInstanceWithToken(token).get(
       `/users?page=${currentPage}&count=${pageSize}`,
     );
   },
 };
 export const followAPI = {
-  follow({ id, token }) {
+  follow({ id, token }: { id: string, token: string }) {
     return makeInstanceWithToken(token).post('follow', { id });
   },
-  approve({ id, token }) {
+  approve({ id, token }: { id: string, token: string }) {
     return makeInstanceWithToken(token).put('approve', { id });
   },
-  unfollow({ id, token }) {
+  unfollow({ id, token }: { id: string, token: string }) {
     return makeInstanceWithToken(token).put('unfollow', { id });
   },
 };
 
 export const authAPI = {
-  registration(data) {
+  registration(data: { firstName: string, lastName: string, email: string, login: string, password: string }) {
     return baseInstance.post('registration', { ...data });
   },
-  authentication(data) {
+  authentication(data: { login: string, password: string }) {
     return baseInstance.post('login', { ...data });
   },
 };
