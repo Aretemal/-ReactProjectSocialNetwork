2dfdabda51052583399bd8af4d9ecad490c9d193
Aretemal <arproggamoi@gmail.com>
2023-05-03 23:16:28 +0300

    SN-FRONT-28 feat: update hook useSelector and useDispatch, add types.d.ts, tsconfig.json

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -3,3 +3,7 @@ declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.webp';
+declare module '*.css' {
+    const content: Record<string, string>;
+    export default content;
+};
