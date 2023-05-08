b598a84d390807f5d0557f4fe4fc80727267e7f0
Aretemal <arproggamoi@gmail.com>
2023-05-08 23:41:39 +0300

    SN-FRONT-24 feat: update structer of store

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
-import store from './store/store';
+import store, { persistor } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
