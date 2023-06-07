f6ff03f8d96d882b769f6a576a8225470ff554e5
Aretemal <arproggamoi@gmail.com>
2023-06-07 17:47:06 +0300

    SN-FRONT-31 feat: add info between users

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -1,18 +1,38 @@
 import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 import { withAuthRedirect } from '../../hoc/WithAuthRedirect';
 import { getAllPosts } from '../../store/slices/postSlice';
 import { getProfile } from '../../store/slices/profileSlice';
 import Profile from './Profile';
 import { useAppDispatch, useAppSelector } from '../../hook/hook';
+import { getLang } from '../../store/slices/settingsSlice';
 
 const ProfileContainer: React.FC = () => {
-  const { token } = useAppSelector((state) => state.auth);
+  const { token, authId } = useAppSelector((state) => state.auth);
+  const { lang } = useAppSelector((state) => state.settings);
+  const { id } = useAppSelector((state) => state.profile);
   const dispatch = useAppDispatch();
+  const { i18n } = useTranslation();
 
   useEffect(() => {
-    dispatch(getProfile(token));
-    dispatch(getAllPosts(token));
-  }, [dispatch, token]);
+    if (!id) {
+      dispatch(getProfile({ token, id: authId }));
+      dispatch(getAllPosts({ token, id: authId }));
+      dispatch(getLang(token));
+    } else if (id === authId) {
+      dispatch(getProfile({ token, id: authId }));
+      dispatch(getAllPosts({ token, id: authId }));
+      dispatch(getLang(token));
+    } else {
+      dispatch(getProfile({ token, id }));
+      dispatch(getAllPosts({ token, id }));
+      dispatch(getLang(token));
+    }
+  }, [id]);
+
+  useEffect(() => {
+    i18n.changeLanguage(lang);
+  }, [lang]);
 
   return (
     <Profile />
