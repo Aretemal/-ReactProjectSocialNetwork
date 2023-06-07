f183144962a57bcf339eca967607ab6755552ca8
Aretemal <arproggamoi@gmail.com>
2023-06-07 17:52:58 +0300

    SN-FRONT-31 feat: add connection between users

--- src/components/Profile/Contact/ContactContainer.tsx
+++ src/components/Profile/Contact/ContactContainer.tsx
@@ -1,23 +1,36 @@
 import React, { useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import { useAppDispatch, useAppSelector } from '../../../hook/hook';
 import { getUsersForProfile } from '../../../store/slices/contactSlice';
 import Contact from './Contact';
 import styles from './Contact.module.css';
+import { setId } from '../../../store/slices/profileSlice';
 
 const ContactContainer: React.FC = () => {
-  const { token } = useAppSelector((state) => state.auth);
+  const { token, authId } = useAppSelector((state) => state.auth);
+  const { id } = useAppSelector((state) => state.profile);
   const { friends, subscribers, subscriptions } = useAppSelector((state) => state.contact);
   const dispatch = useAppDispatch();
+  const { t } = useTranslation();
 
   useEffect(() => {
-    dispatch(getUsersForProfile(token));
-  }, []);
+    if (!id) {
+      dispatch(getUsersForProfile({ token, id: authId }));
+    } else {
+      dispatch(getUsersForProfile({ token, id }));
+    }
+  }, [id]);
+  const onUser = ({ profileId, login }: { profileId: string, login: string }) => {
+    dispatch(setId(profileId));
+    return <Navigate to={`/profile/${login}`} />;
+  };
 
   return (
     <div className={styles.container}>
-      <Contact title="Your friends" users={friends} />
-      <Contact title="Your subscribers" users={subscribers} />
-      <Contact title="Your subscriptions" users={subscriptions} />
+      <Contact title={t('Friends')} users={friends} onUser={onUser} />
+      <Contact title={t('Subscribers')} users={subscribers} onUser={onUser} />
+      <Contact title={t('Subscriptions')} users={subscriptions} onUser={onUser} />
     </div>
   );
 };
--- src/components/Profile/Contact/ContactItem/ContactItem.tsx
+++ src/components/Profile/Contact/ContactItem/ContactItem.tsx
@@ -1,15 +1,26 @@
+/* eslint-disable react/no-unused-prop-types */
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import DefaultAva from '../../../../assets/images/DefaultAva.webp';
 import styles from '../Contact.module.css';
 
 interface IProps {
     firstName: string,
     lastName: string,
+    onUser: ({ profileId, login }: { profileId: string, login: string }) => any,
+    id: string,
+    login: string,
 }
-const ContactItem: React.FC<IProps> = ({ firstName, lastName }) => (
+const ContactItem: React.FC<IProps> = ({
+  firstName, lastName, id, onUser, login,
+}) => (
   <div className={styles.itemContainer}>
-    <img className={styles.ava} src={DefaultAva} alt="ava" />
-    <span>{`${firstName} ${lastName}`}</span>
+    <NavLink to={`/profile/${login}`} onClick={() => onUser({ profileId: id, login })}>
+      <img className={styles.ava} src={DefaultAva} alt="ava" />
+    </NavLink>
+    <div>{`${firstName} ${lastName}`}</div>
   </div>
 );
 export default ContactItem;
