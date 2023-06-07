09581b657933c5c88506bc13dc78429486274982
Aretemal <arproggamoi@gmail.com>
2023-06-07 18:00:34 +0300

    SN-FRONT-31 feat: update Profile

--- src/store/slices/interfaces/profileInterface.ts
+++ src/store/slices/interfaces/profileInterface.ts
@@ -1,10 +1,12 @@
 export interface IProfile {
+    id: string,
     profile: {
         firstName: string,
         lastName: string,
         status: string,
         login: string,
         email: string,
+        hasConnection: string,
     }
 }
 
--- src/store/slices/profileSlice.ts
+++ src/store/slices/profileSlice.ts
@@ -4,12 +4,14 @@ import { IProfile, IUpdateStatus } from './interfaces/profileInterface';
 import { AppDispatch } from '../store';
 
 const initialState: IProfile = {
+  id: '',
   profile: {
     login: '',
     firstName: '',
     lastName: '',
     status: '',
     email: '',
+    hasConnection: '',
   },
 };
 const profileSlice = createSlice({
@@ -18,19 +20,28 @@ const profileSlice = createSlice({
   reducers: {
     setProfile: (state, action: PayloadAction<IProfile>) => {
       state.profile = action.payload.profile;
+      state.id = action.payload.id;
     },
     setStatus: (state, action: PayloadAction<string>) => {
       state.profile.status = action.payload;
     },
+    setId: (state, action: PayloadAction<string>) => {
+      state.id = action.payload;
+    },
+    setLogin: (state, action: PayloadAction<string>) => {
+      state.profile.login = action.payload;
+    },
   },
 });
-export const { setStatus, setProfile } = profileSlice.actions;
+export const {
+  setStatus, setLogin, setProfile, setId,
+} = profileSlice.actions;
 export default profileSlice.reducer;
-export const getProfile = createAsyncThunk<void, string, { dispatch: AppDispatch }>(
+export const getProfile = createAsyncThunk<void, { token: string, id: string }, { dispatch: AppDispatch }>(
   'profile/getProfile',
-  async (token, { dispatch }) => {
-    const response = await profileAPI.getInfoAuthUser(token);
-    dispatch(setProfile({ profile: response.data.data.attributes }));
+  async (data, { dispatch }) => {
+    const response = await profileAPI.getOne(data);
+    dispatch(setProfile({ profile: response.data.data.attributes, id: response.data.data.id }));
   },
 );
 export const updateStatus = createAsyncThunk<void, IUpdateStatus, { dispatch: AppDispatch }>(
